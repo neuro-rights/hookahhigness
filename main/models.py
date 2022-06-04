a245ab6449b341617cceef433663218dbfc2a30d
strikles <strikles@gmail.com>
2022-06-04 22:15:50 +0200

    styling

--- main/models.py
+++ main/models.py
@@ -58,7 +58,7 @@ class NFTCollection(models.Model):
 
     @property
     def get_random_nft(self):
-        return self.nfts.all()[:3][::-1]
+        return self.nfts.order_by("?").first()
 
     #
     def get_absolute_url(self):
