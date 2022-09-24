68831944365d7e3cd48754b48599c6c3216f19ca
x7E3 <x7E3@icloud.com>
2022-09-24 15:15:49 +0200

    mint

--- main/view_parts/nfts/nft_functions.py
+++ main/view_parts/nfts/nft_functions.py
@@ -1,6 +1,13 @@
 import boto3
+import time
 import uuid
 
+from web3 import Web3, HTTPProvider
+
+human_0 = '0x6A48A50A53462A22D2c30F5138c4970a3020b30a'
+human_address_1 = "0x92e320aE601BC8235D605d38D03142C3FE28Ba92"
+foggy = '0xa456c84CC005100B277D4637896456F99a59A290'
+sarit = '0xB795518Ee574c2a55B513D2C1319e8e6e40F6c04'
 
 # Import the login_required decorator
 from django.contrib.auth.decorators import login_required
@@ -21,6 +28,27 @@ from ...models import Asset, Auction, Nft
 #
 from ..utils.pagination import *
 
+
+def mint_nft(request, nft_uuid):
+    """ """
+
+    w3 = Web3(HTTPProvider('http://localhost:8545'))  # web3 must be called locally
+    assert True is w3.isConnected()
+    print('Network connected')
+    address = "0x3194cBDC3dbcd3E11a07892e7bA5c3394048Cc87"
+    # To get ABI
+    # vyper -f abi contracts/ERC721.vy
+    abi = '[{"name": "Transfer", "inputs": [{"name": "sender", "type": "address", "indexed": true}, {"name": "receiver", "type": "address", "indexed": true}, {"name": "tokenId", "type": "uint256", "indexed": true}], "anonymous": false, "type": "event"}, {"name": "Approval", "inputs": [{"name": "owner", "type": "address", "indexed": true}, {"name": "approved", "type": "address", "indexed": true}, {"name": "tokenId", "type": "uint256", "indexed": true}], "anonymous": false, "type": "event"}, {"name": "ApprovalForAll", "inputs": [{"name": "owner", "type": "address", "indexed": true}, {"name": "operator", "type": "address", "indexed": true}, {"name": "approved", "type": "bool", "indexed": false}], "anonymous": false, "type": "event"}, {"stateMutability": "nonpayable", "type": "constructor", "inputs": [], "outputs": []}, {"stateMutability": "pure", "type": "function", "name": "supportsInterface", "inputs": [{"name": "interface_id", "type": "bytes4"}], "outputs": [{"name": "", "type": "bool"}]}, {"stateMutability": "view", "type": "function", "name": "balanceOf", "inputs": [{"name": "_owner", "type": "address"}], "outputs": [{"name": "", "type": "uint256"}]}, {"stateMutability": "view", "type": "function", "name": "ownerOf", "inputs": [{"name": "_tokenId", "type": "uint256"}], "outputs": [{"name": "", "type": "address"}]}, {"stateMutability": "view", "type": "function", "name": "getApproved", "inputs": [{"name": "_tokenId", "type": "uint256"}], "outputs": [{"name": "", "type": "address"}]}, {"stateMutability": "view", "type": "function", "name": "isApprovedForAll", "inputs": [{"name": "_owner", "type": "address"}, {"name": "_operator", "type": "address"}], "outputs": [{"name": "", "type": "bool"}]}, {"stateMutability": "payable", "type": "function", "name": "transferFrom", "inputs": [{"name": "_from", "type": "address"}, {"name": "_to", "type": "address"}, {"name": "_tokenId", "type": "uint256"}], "outputs": []}, {"stateMutability": "payable", "type": "function", "name": "safeTransferFrom", "inputs": [{"name": "_from", "type": "address"}, {"name": "_to", "type": "address"}, {"name": "_tokenId", "type": "uint256"}], "outputs": []}, {"stateMutability": "payable", "type": "function", "name": "safeTransferFrom", "inputs": [{"name": "_from", "type": "address"}, {"name": "_to", "type": "address"}, {"name": "_tokenId", "type": "uint256"}, {"name": "_data", "type": "bytes"}], "outputs": []}, {"stateMutability": "payable", "type": "function", "name": "approve", "inputs": [{"name": "_approved", "type": "address"}, {"name": "_tokenId", "type": "uint256"}], "outputs": []}, {"stateMutability": "nonpayable", "type": "function", "name": "setApprovalForAll", "inputs": [{"name": "_operator", "type": "address"}, {"name": "_approved", "type": "bool"}], "outputs": []}, {"stateMutability": "nonpayable", "type": "function", "name": "mint", "inputs": [{"name": "_to", "type": "address"}, {"name": "_tokenId", "type": "uint256"}], "outputs": [{"name": "", "type": "bool"}]}, {"stateMutability": "nonpayable", "type": "function", "name": "burn", "inputs": [{"name": "_tokenId", "type": "uint256"}], "outputs": []}, {"stateMutability": "view", "type": "function", "name": "tokenURI", "inputs": [{"name": "tokenId", "type": "uint256"}], "outputs": [{"name": "", "type": "string"}]}]'
+    contract_instance = w3.eth.contract(address=address, abi=abi)
+    # Both raise error
+    human_0 = "0x2BBF135e8E7D2aA3eF1Ab949548f4E4d8Fa7db16"
+    human_0_balance = contract_instance.functions.balanceOf(human_0).call({'from': foggy})
+    # Change tokenId to avoid error
+    token_id = 15
+    result = contract_instance.functions.mint(human_0, token_id).transact({'from': human_0})
+    time.sleep(60)
+    new_human_0_balance = contract_instance.functions.balanceOf(human_0).call({'from': foggy})
+    assert new_human_0_balance == human_0_balance + 1
 
 
 def search_result(request):
