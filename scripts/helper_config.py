ea1c04f3fddedf34b88e4e014e415437d9dc316e
x7E3 <x7E3@icloud.com>
2022-09-25 11:20:27 +0200

    Lottery.vy

---
+++ scripts/helper_config.py
@@ -0,0 +1,26 @@
+network_config = {
+    "ethereum": {
+        "local": {
+            "subscription_id": 0,
+            "key_hash": "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+            "update_interval": 60,
+            "fund_amount": 5_000000000000000000,
+        },
+        "mainnet-fork": {
+            "subscription_id": 0,
+            "key_hash": "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+            "update_interval": 60,
+            "fund_amount": 5_000000000000000000,
+        },
+        "goerli": {
+            "subscription_id": 9757,  # Add your own subscription Id here!
+            "key_hash": "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+            "update_interval": 60,
+            "fund_amount": 5_000000000000000000,
+        },
+    }
+}
+
+
+def main():
+    pass
