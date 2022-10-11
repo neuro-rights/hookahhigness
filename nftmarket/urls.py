b46b827019acea9aee340ac6894ccdb0aaa6167d
x7E3 <x7E3@icloud.com>
2022-10-11 11:37:26 +0200

    start metamask sso

--- nftmarket/urls.py
+++ nftmarket/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('metamask/', include('metaMaskAuth.urls')),
     path('', include('main.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
