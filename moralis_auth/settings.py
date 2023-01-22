3455303fd6521bfd26381b95a593b0eff24e8f24
neuro-rights <106665209+neuro-rights@users.noreply.github.com>
2023-01-22 17:19:08 +0100

    heroku

--- moralis_auth/settings.py
+++ moralis_auth/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     "taggit",
     "mathfilters",
     "django_bootstrap5",
